4a76ce4028d5bacc31430fa4016c969d9eb7ee75
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-08-05 18:34:28 +0300

    Добавил маршруты / и /posts/{id:\d+}

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,13 +2,34 @@
 
 namespace Somecode\Framework\Http;
 
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
     public function handle(Request $request): Response
     {
-        // example: controller -> $content
-        $content = '<h1>Hello, World!</h1>';
+        $dispatcher = simpleDispatcher(function (RouteCollector $collector) {
+            $collector->get('/', function () {
+                $content = '<h1>Some content</h1>';
 
-        return new Response($content);
+                return new Response($content);
+            });
+
+            $collector->get('/posts/{id:\d+}', function (array $vars) {
+                $content = "<h1>Post - {$vars['id']}</h1>";
+
+                return new Response($content);
+            });
+        });
+
+        $routeInfo = $dispatcher->dispatch(
+            $request->server['REQUEST_METHOD'],
+            $request->server['REQUEST_URI'],
+        );
+
+        [$status, $handler, $vars] = $routeInfo;
+
+        return $handler($vars);
     }
 }
