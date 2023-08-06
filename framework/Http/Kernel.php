718e6f2db71edc13dc6e812005e1de778949687d
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-08-06 13:32:18 +0300

    Создал класс Router и вынес в него основную логику

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,41 +2,24 @@
 
 namespace Somecode\Framework\Http;
 
-use FastRoute\RouteCollector;
-use function FastRoute\simpleDispatcher;
+use Somecode\Framework\Routing\RouterInterface;
 
 class Kernel
 {
+    public function __construct(
+        private RouterInterface $router
+    ) {
+    }
+
     public function handle(Request $request): Response
     {
-        $dispatcher = simpleDispatcher(function (RouteCollector $collector) {
-            $routes = include BASE_PATH.'/routes/web.php';
+        try {
+            [$routeHandler, $vars] = $this->router->dispatch($request);
 
-            foreach ($routes as $route) {
-                $collector->addRoute(...$route);
-            }
-
-            //            $collector->get('/', function () {
-            //                $content = '<h1>Some content</h1>';
-            //
-            //                return new Response($content);
-            //            });
-            //
-            //            $collector->get('/posts/{id:\d+}', function (array $vars) {
-            //                $content = "<h1>Post - {$vars['id']}</h1>";
-            //
-            //                return new Response($content);
-            //            });
-        });
-
-        $routeInfo = $dispatcher->dispatch(
-            $request->getMethod(),
-            $request->getPath(),
-        );
-
-        [$status, [$controller, $method], $vars] = $routeInfo;
-
-        $response = call_user_func_array([new $controller, $method], $vars);
+            $response = call_user_func_array($routeHandler, $vars);
+        } catch (\Throwable $exception) {
+            $response = new Response($exception->getMessage(), 500);
+        }
 
         return $response;
     }
