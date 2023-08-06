1e2385170bcd13c9926b5018a50048f37d98f9a6
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-08-06 12:06:28 +0300

    Добавил методы getPath() и getMethod() в класс Request

--- framework/Http/Request.php
+++ framework/Http/Request.php
@@ -9,12 +9,22 @@ class Request
         private readonly array $postData,
         private readonly array $cookies,
         private readonly array $files,
-        public readonly array $server,
+        private readonly array $server,
     ) {
     }
 
     public static function createFromGlobals(): static
     {
         return new static($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+    }
+
+    public function getPath(): string
+    {
+        return strtok($this->server['REQUEST_URI'], '?');
+    }
+
+    public function getMethod(): string
+    {
+        return $this->server['REQUEST_METHOD'];
     }
 }
