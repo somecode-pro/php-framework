72c18126fdeb4f7b527f68fac9febf01089f7d9c
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-08-06 14:03:36 +0300

    Добавил обработку "not found" и "method not allowed"

--- framework/Routing/Router.php
+++ framework/Routing/Router.php
@@ -2,13 +2,25 @@
 
 namespace Somecode\Framework\Routing;
 
+use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
+use Somecode\Framework\Http\Exceptions\MethodNotAllowedException;
+use Somecode\Framework\Http\Exceptions\RouteNotFoundException;
 use Somecode\Framework\Http\Request;
 
 class Router implements RouterInterface
 {
     public function dispatch(Request $request): array
+    {
+        [$handler, $vars] = $this->extractRouteInfo($request);
+
+        [$controller, $method] = $handler;
+
+        return [[new $controller, $method], $vars];
+    }
+
+    private function extractRouteInfo(Request $request): array
     {
         $dispatcher = simpleDispatcher(function (RouteCollector $collector) {
             $routes = include BASE_PATH.'/routes/web.php';
@@ -23,8 +35,14 @@ class Router implements RouterInterface
             $request->getPath(),
         );
 
-        [$status, [$controller, $method], $vars] = $routeInfo;
-
-        return [[new $controller, $method], $vars];
+        switch ($routeInfo[0]) {
+            case Dispatcher::FOUND:
+                return [$routeInfo[1], $routeInfo[2]];
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = implode(', ', $routeInfo[1]);
+                throw new MethodNotAllowedException("Supported HTTP methods: $allowedMethods");
+            default:
+                throw new RouteNotFoundException('Route not found');
+        }
     }
 }
