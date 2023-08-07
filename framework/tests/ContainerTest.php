a1808de77389e2a25c5cb8f7c4e9ca2bc05acbe6
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-08-07 20:51:07 +0300

    Реализовал метод has()

--- framework/tests/ContainerTest.php
+++ framework/tests/ContainerTest.php
@@ -25,4 +25,14 @@ class ContainerTest extends TestCase
 
         $container->add('no-class');
     }
+
+    public function test_has_method()
+    {
+        $container = new Container();
+
+        $container->add('somecode-class', SomecodeClass::class);
+
+        $this->assertTrue($container->has('somecode-class'));
+        $this->assertFalse($container->has('no-class'));
+    }
 }
