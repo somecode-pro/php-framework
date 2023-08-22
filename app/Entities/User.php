3d1c8ac006d5c98aa471f5aa540032e600695ce6
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-08-22 17:26:25 +0300

    Создал сущность для пользователя

---
+++ app/Entities/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entities;
+
+class User
+{
+    public function __construct(
+        private ?int $id,
+        private ?string $name,
+        private string $email,
+        private string $password,
+        private \DateTimeImmutable $createAt
+    ) {
+    }
+
+    public static function create(string $email, string $password, \DateTimeImmutable $createdAt = null, string $name = null, int $id = null): static
+    {
+        return new static($id, $name, $email, $password, $createdAt ?? new \DateTimeImmutable());
+    }
+}
