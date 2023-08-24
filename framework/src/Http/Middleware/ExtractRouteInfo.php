2d499da6d29c6cdfe7995ab8ffbe83e1574fa05f
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-08-24 10:43:00 +0300

    Завершил рефакторинг класса Router

--- framework/src/Http/Middleware/ExtractRouteInfo.php
+++ framework/src/Http/Middleware/ExtractRouteInfo.php
@@ -46,5 +46,7 @@ class ExtractRouteInfo implements MiddlewareInterface
                 $e->setStatusCode(404);
                 throw $e;
         }
+
+        return $handler->handle($request);
     }
 }
