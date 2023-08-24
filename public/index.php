f10dfcfd754d198cda819ac00ba431160772841c
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-08-24 17:13:47 +0300

    Связывание компонентов событий через EventDispatcher

--- public/index.php
+++ public/index.php
@@ -12,6 +12,13 @@ $request = Request::createFromGlobals();
 /** @var \League\Container\Container $container */
 $container = require BASE_PATH.'/config/services.php';
 
+$eventDispatcher = $container->get(\Somecode\Framework\Event\EventDispatcher::class);
+$eventDispatcher
+    ->addListener(
+        \Somecode\Framework\Http\Events\ResponseEvent::class,
+        new \App\Listeners\ContentLengthListener()
+    );
+
 $kernel = $container->get(Kernel::class);
 
 $response = $kernel->handle($request);
